f37fa7ec097eec1d71c34a1d244ad638c3929f01
Huong Pham <96hpham@gmail.com>
2022-11-07 16:40:06 -0600

    Modified db.py

---
+++ db.py
@@ -0,0 +1,47 @@
+depts = {
+    "Bakery":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "Breakfast":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "Beauty":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "Fresh":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "Frozen":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "GM-Household":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "Home Office":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    },
+    "Health":{
+        "subcategories":[],
+        "brands":[],
+        "items":{}
+    }
+}
+items = {
+    "all": {}
+    # id :{ "uuid": "xyz", "name": "sdan", "brand":"private","price":X.YZ,"weekly_movement":X.YZ }
+
+}
